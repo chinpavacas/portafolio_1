2d5930b7866ca1620b43fc8a6b756329145d013b
Richard Castiblanco <106503816+chinpavacas@users.noreply.github.com>
2023-08-27 07:34:27 -0500

    Add files via upload

---
+++ mi portafolio/logica.js
@@ -0,0 +1,10 @@
+function mostrar(){
+    Swal.fire({
+        title: 'MI NUMERO DE CONTACTO!',
+        text: '+57 3025453611',
+        imageUrl: 'what.png',
+        imageWidth: 100,
+        imageHeight: 100,
+        imageAlt: 'Custom image',
+      });
+};
